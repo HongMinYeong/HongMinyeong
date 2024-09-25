f6e917be2ab72c84952ae4497b9c6cc5e3ee8c24
hongminyeong <65701100+HongMinYeong@users.noreply.github.com>
2024-09-25 14:56:25 +0900

    Update HongMinyeong.py

--- HongMinyeong.py
+++ HongMinyeong.py
@@ -20,35 +20,60 @@ markdown_text = """<p align="center">
 
 </p>
 
-<div align="center">
-  <h3> ✨ T e c h ✨ </h3> 
-  
-  <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=flat-square&logo=JavaScript&logoColor=black&label=">
-  <img src="https://img.shields.io/badge/HTML5-E34F26?style=flat-square&logo=HTML5&logoColor=white&label=">
-  <img src="https://img.shields.io/badge/CSS-1572B6?style=flat-square&logo=CSS3&logoColor=white&label=">
-  <img src="https://img.shields.io/badge/React-61DAFB?style=flat-square&logo=React&logoColor=white&label=">
-
-  <br>
-  <img src="https://img.shields.io/badge/Node.js-339933?style=flat-square&logo=Node.js&logoColor=white&label=">
-  <img src="https://img.shields.io/badge/Python-3776AB?style=flat-square&logo=Python&logoColor=white&label=">
-  <img src="https://img.shields.io/badge/Express.js-404D59?style=flat-square&label=">
-  <img src="https://img.shields.io/badge/Java-ED8B00?style=flat-square&logo=java&logoColor=white&label=">
-  <img src="https://img.shields.io/badge/TypeScript-007ACC?style=flat-square&logo=typescript&logoColor=white&label=">
-  <img src="https://img.shields.io/badge/spring-6DB33F?style=flat-square&logo=spring&logoColor=white&label=">
-  <br>
-  <img src="https://img.shields.io/badge/MySQL-00000F?style=flat-square&logo=mysql&logoColor=white&label=">
-  <img src="https://img.shields.io/badge/MongoDB-4EA94B?style=flat-square&logo=mongodb&logoColor=white&label=">
-  <img src="https://img.shields.io/badge/Postman-FF6C37?style=flat-square&logo=Postman&logoColor=white"/>
-  <img src="https://img.shields.io/badge/ORACLE-F80000?style=flat-square&logo=oracle&logoColor=white"/>
-  <img src="https://img.shields.io/badge/Linux-FCC624?style=flat-square&logo=linux&logoColor=black"/>
-  <img src="https://img.shields.io/badge/GitHub-181717?style=flat-square&logo=GitHub&logoColor=white"/>
-  <img src="https://img.shields.io/badge/Amazon AWS-232F3E?style=flat-square&logo=amazonaws&logoColor=white"/>
-<img src="https://img.shields.io/badge/Anaconda-44A833?style=flat-square&logo=Anaconda&logoColor=white"/>
-<br/>
-  <br/>
+[![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2FHongMinYeong&count_bg=%23E92E5D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false)](https://hits.seeyoufarm.com)
+<br>
 <hr>
-<br/>
-  <br/>
+<div align="center">
+   <h2> ⚒️ T e c h ⚒️ </h2> 
+
+  <h3>ServerSide</h3>
+  <img src="https://img.shields.io/badge/Node.js-339933?style=flat&logo=Node.js&logoColor=white">
+  <img src="https://img.shields.io/badge/Express.js-404D59?style=flat&logo=Express&logoColor=white">
+  <img src="https://img.shields.io/badge/spring-6DB33F?style=flat&logo=spring&logoColor=white">
+  
+  <h3>DataBase</h3>
+  <img src="https://img.shields.io/badge/MySQL-00000F?style=flat&logo=mysql&logoColor=white">
+  <img src="https://img.shields.io/badge/MongoDB-4EA94B?style=flat&logo=mongodb&logoColor=white">
+ 
+  <h3>ORM/ODM</h3>
+  <img src="https://img.shields.io/badge/Sequelize-52B0E7?style=flat&logo=sequelize&logoColor=white">
+  <img src="https://img.shields.io/badge/JPA-007396?style=flat&logo=Java&logoColor=white">
+  <img src="https://img.shields.io/badge/Mongoose-47A248?style=flat&logo=mongoose&logoColor=white">
+  
+  <h3>Deploy</h3>
+  <img src="https://img.shields.io/badge/Docker-2496ED?style=flat&logo=Docker&logoColor=white">
+  <img src="https://img.shields.io/badge/AWS%20EC2-232F3E?style=flat&logo=Amazon%20AWS&logoColor=white">
+<!--   <img src="https://img.shields.io/badge/AWS%20ECS-232F3E?style=flat&logo=Amazon%20AWS&logoColor=white"> -->
+  <img src="https://img.shields.io/badge/S3%20Bucket-569A31?style=flat&logo=Amazon%20S3&logoColor=white">
+  <h3>Programing Language</h3>
+  <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=flat&logo=JavaScript&logoColor=black">
+  <img src="https://img.shields.io/badge/Java-804000?style=flat&logo=OpenJDK&logoColor=white"/>
+  <img src="https://img.shields.io/badge/TypeScript-007ACC?style=flat&logo=typescript&logoColor=white">
+  <img src="https://img.shields.io/badge/C-A8B9CC?style=flat&logo=C&logoColor=black">
+  <img src="https://img.shields.io/badge/Python-3776AB?style=flat&logo=Python&logoColor=white">
+  <h3>FE</h3>
+  <img src="https://img.shields.io/badge/HTML5-E34F26?style=flat&logo=HTML5&logoColor=white">
+  <img src="https://img.shields.io/badge/CSS-1572B6?style=flat&logo=CSS3&logoColor=white">
+  <img src="https://img.shields.io/badge/React-61DAFB?style=flat&logo=React&logoColor=white">
+  <img src="https://img.shields.io/badge/Next.js-000000?style=flat&logo=Next.js&logoColor=white">
+  <img src="https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=flat&logo=Tailwind%20CSS&logoColor=white">
+  <h3>Tools</h3>
+  <img src="https://img.shields.io/badge/Postman-FF6C37?style=flat&logo=Postman&logoColor=white">
+  <img src="https://img.shields.io/badge/DBeaver-000000?style=flat&logo=DBeaver&logoColor=white">
+  <img src="https://img.shields.io/badge/Notion-000000?style=flat&logo=Notion&logoColor=white">
+  <h3>IDE</h3>
+<img src="https://img.shields.io/badge/Visual_Studio_Code-007ACC?style=flat&logo=Visual%20Studio%20Code&logoColor=white">
+<img src="https://img.shields.io/badge/IntelliJ_IDEA-000000?style=flat&logo=IntelliJ%20IDEA&logoColor=white">
+  </div>
+  
+  <hr>
+  
+  <div align="center">
+    <h2>🔗 프로젝트 모음 레포지토리</h2><p>모든 프로젝트를 한 곳에서 보고 싶으신가요? <a href="https://github.com/HongMinYeong/HongMYProjects">여기</a>에서 확인하세요!</p>
+
+  </div>
+  
+  <hr>
 <h3 align="center">✨ B l o g & S o c i a l ✨</h3>
 <p align="center">
   <a href="https://0boss.tistory.com/"><img src="http://img.shields.io/badge/-Tistory%20[KR]-black?style=flat-square&logo=tistory&link=https://dksl00.tistory.com/" /></a>&nbsp;
@@ -67,46 +92,24 @@ markdown_text = """<p align="center">
 """
 markdown_text2 = """
 
+<div align="center">
+    <h2>✨ Blog & Social ✨</h2>
+    <p>
+      <a href="https://0boss.tistory.com/">
+        <img src="http://img.shields.io/badge/-Tistory%20[KR]-black?style=flat-square&logo=tistory&link=https://0boss.tistory.com/" alt="Tistory">
+      </a>
+      <br/>
+      <a href="mailto:minyung1240@khu.ac.kr">
+        <img src="https://img.shields.io/badge/Gmail-d14836?style=flat-square&logo=Gmail&logoColor=white&link=mailto:minyung1240@khu.ac.kr" alt="Gmail">
+      </a>
+    </p>
+  </div>
+  
 <br/>
   <br/>
-<hr>
-<br/>
-  <br/>
-<h3> ✨ P r o j e c t s✨ </h3> 
 
-__:memo: [Ahwhew] Next.js + Spring Boot를 이용한 신세 한탄 AI 그림 일기 서비스__
-
-<a href="https://github.com/sessac-3rd-team-A/BE"><img src="https://github.com/sy33002/sy33002/assets/113359008/fcc3fa51-f39b-4e50-80e8-7de146844e91" width=400 height=300> </a>
-<br /><br />
-__:memo: [열줌쉬어] : React.js, Node.js(Express), TypeScript를 활용한 웹 풀스택 프로젝트__
-
-<a href="https://github.com/WebDeViper/WebDeViper_Server"><img src="https://github.com/HongMinYeong/HongMinyeong/assets/65701100/9dea4773-eb51-4abd-9d2a-5268a9723aab" width=400 height=300> </a>
-
-
-<br /><br />
-
-__:memo: [송편지] : node.js + express를 이용한 온라인 롤링페이퍼 사이트__
-
-<a href="https://github.com/sesacproj1/A_Team_Proj"><img src="https://github.com/HongMinYeong/HongMinyeong/assets/65701100/9b778f57-115c-4ba1-bec4-a1512797816e" width=400 height=300> </a>
-
-<br />
-  <br>
-  <hr>
-  <h2> .. 더보기 </h2>
-  <h3> ஓ๑⸜❤︎⸝‍๑ஓ </h3> 
-  <h3>  ᕬ ᕬ   ∧ ∧ </h3> 
-<h3> (˵ㅇ◡ㅇ˵)  (ᓀ ֊ ᓂ˵ )</h3> 
-<h3>  (つ🍺O ⠀O🍺⊂) </h3> 
-
- 
-[![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2FHongMinYeong&count_bg=%23E92E5D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false)](https://hits.seeyoufarm.com)
-<br>
-
-[![GitHub Streak](https://streak-stats.demolab.com?user=HongMinYeong&hide_border=true)](https://git.io/streak-stats)
-<br>
 [![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=HongMinYeong&layout=compact&theme=Most%20Used%20Languages&langs_count=6)](https://github.com/anuraghazra/github-readme-stats)
 <br>
-[![Anurag's github stats](https://github-readme-stats.vercel.app/api?username=HongMinYeong)](https://github.com/anuraghazra/github-readme-stats)
 
 </div>
 """
